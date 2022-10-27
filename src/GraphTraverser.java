c3a1ef82ac8107f26f21a9160a8d087e2f920a2f
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2022-10-27 11:57:35 -0400

    remove

--- src/GraphTraverser.java
+++ src/GraphTraverser.java
@@ -1,10 +1,7 @@
 public class GraphTraverser {
 
     public static void main(String[] args) {
-        System.out.println("Test 1");
-        System.out.println("Test 2");
-        System.out.println("Test 3");
-//        comment
+
     }
 
 }

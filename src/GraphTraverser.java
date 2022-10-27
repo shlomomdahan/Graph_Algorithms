c4840ddc34d2500a4339f2acc2f0788d88aa1dda
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2022-10-27 13:26:48 -0400

    Depth First Search

--- src/GraphTraverser.java
+++ src/GraphTraverser.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+
 public class GraphTraverser {
 
+    public static void depthFirstTraversal(Vertex start, ArrayList<Vertex> visitedVertices) {
+        System.out.println(start.getData());
+
+        for (Edge e : start.getEdges()) {
+            Vertex neighbor = e.getEnd();
+
+            if (!visitedVertices.contains(neighbor)) {
+                visitedVertices.add(neighbor);
+                GraphTraverser.depthFirstTraversal(neighbor, visitedVertices);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        TestGraph test = new TestGraph();
+        Vertex startingVertex = test.getStartingVertex();
+        ArrayList<Vertex> visitedVertices = new ArrayList<>();
+        visitedVertices.add(startingVertex);
+
+        GraphTraverser.depthFirstTraversal(startingVertex, visitedVertices);
+
 
     }
 

981c690bbb20e97bd470f751f4e32c6307ba426a
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2022-10-27 11:55:08 -0400

    Add print test3

--- src/GraphTraverser.java
+++ src/GraphTraverser.java
@@ -3,6 +3,7 @@ public class GraphTraverser {
     public static void main(String[] args) {
         System.out.println("Test 1");
         System.out.println("Test 2");
+        System.out.println("Test 3");
     }
 
 }
